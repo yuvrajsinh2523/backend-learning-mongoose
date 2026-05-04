3e28f0c6b3b5b5ace322de938edc7cf3faa08595
yuvrajsinh jadeja <yuvrajsinhjadeja25023@gmail.com>
2026-05-04 22:26:04 +0530

    Remove 'new' option from updateOne method

--- MONGO/index.js
+++ MONGO/index.js
@@ -56,7 +56,7 @@ User.findOne({name:"vishal"}).then((data)=>{
 User.findById("69f87278fbba8bb1d5a24007").then((data)=>{
     console.log(data.name);
 })
-User.updateOne({name:"vishal"},{age:25},{new:true}).then((data)=>{
+User.updateOne({name:"vishal"},{age:25}).then((data)=>{
     console.log(data);
 })
 
